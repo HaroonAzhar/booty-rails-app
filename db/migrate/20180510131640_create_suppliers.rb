708423904aa64f414a1799d9b5a2dfd7a9675222
Haroon Azhar <blazintee15@gmail.com>
2018-05-16 00:05:57 +0500

    migrations editted

--- db/migrate/20180510131640_create_suppliers.rb
+++ db/migrate/20180510131640_create_suppliers.rb
@@ -2,6 +2,7 @@ class CreateSuppliers < ActiveRecord::Migration[5.2]
   def change
     create_table :suppliers do |t|
       #add name 
+      t.string, :name
       t.timestamps
     end
   end
--- db/migrate/20180510233441_add_userid_to_item.rb
+++ db/migrate/20180510233441_add_userid_to_item.rb
@@ -3,6 +3,7 @@ class AddUseridToItem < ActiveRecord::Migration[5.2]
   	#quantity
     add_column :items, :shopping_experience_id, :integer
     add_column :items, :category_id, :integer
+    add_column :items, :quantity, :integer
     add_column :items, :supplier_id, :integer
     add_column :items, :cart_id, :integer
   end
